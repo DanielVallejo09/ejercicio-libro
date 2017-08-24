c26b4fc1aa5fe5f5cda15c8e632d4f5766e4840e
daniel vallejo <daniel_vallejoquiin@hotmail.com>
2017-08-24 10:32:45 -0500

    Creamos paso a paso para ingresar autor y libro

---
+++ Libro.java
@@ -0,0 +1,13 @@
+
+public class Libro
+{
+    private String Titulo;
+    private String Autor;
+
+public Libro(String TituloIni, String AutorIni)
+{
+    Titulo = TituloIni;
+    Autor = AutorIni;
+}
+
+}
