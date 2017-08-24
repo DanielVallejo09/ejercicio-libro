1629122508a0991bc3bfa6088563026381c6d810
daniel vallejo <daniel_vallejoquiin@hotmail.com>
2017-08-24 11:01:57 -0500

    Todo listo del ejercicio de evaluacion hasta ahora falta a casa

--- Libro.java
+++ Libro.java
@@ -3,11 +3,13 @@ public class Libro
 {
     private String Titulo;
     private String Autor;
+    private int Paginas;
 
-public Libro(String TituloIni, String AutorIni)
+public Libro(String TituloIni, String AutorIni, int PaginasIni)
 {
     Titulo = TituloIni;
     Autor = AutorIni;
+    Paginas = PaginasIni;
 }
 
 
@@ -20,5 +22,27 @@ public String DimeAutor()
 public String DimeTitulo()
 {
     return Titulo;
+}
+
+public int DimePaginas()
+{
+    return Paginas;
+}
+
+public String RegresaDetalles()
+{
+    String CadRes = " ";
+    
+    CadRes = CadRes + "No.Paginas";
+    CadRes = CadRes + ":";
+    CadRes = CadRes + Paginas;
+    CadRes = CadRes + "Nombre Autor";
+    CadRes = CadRes + ":";
+    CadRes = CadRes + Autor;
+    CadRes = CadRes + "Nombre del Libro";
+    CadRes = CadRes + ":";
+    CadRes = CadRes + Titulo;
+    
+    return CadRes;
 }
 }
